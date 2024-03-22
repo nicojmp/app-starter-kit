91ccdffafdc58e84ff0811ef4dc4f37224783564
nicojmp <157817572+nicojmp@users.noreply.github.com>
2024-03-22 16:28:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.title('🎈 App Name')
 
 st.write('Hello world!')
+
+import streamlit as st  # Importing Streamlit library
+import replicate  # Importing Replicate library
+import os  # Importing the operating system module
